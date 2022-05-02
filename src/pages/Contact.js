80ba152aba96f3b0902925a0973a8322efb62c6b
RyanJ2463 <jacobellr2017@gmail.com>
2022-05-01 23:15:31 -0400

    contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,8 +9,7 @@ const Contact = () => {
     const [state, handleSubmit] = useForm("xwkywwol");
 
 
-    const handleChange = (check) => {
-        console.log(check);
+    const handleChange = () => {
         setCaptchDone(true)
     }
 
@@ -22,38 +21,37 @@ const Contact = () => {
 
   return (
     <div className='contact'>
-        <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-        <ReCAPTCHA
+        <div className="field">
+            <label className='field-text' onSubmit={handleSubmit}>Email Address</label>
+            <input className='email-text'
+                id="email"
+                type="email" 
+                name="email"
+                />
+                <ValidationError 
+                prefix="Email" 
+                field="email"
+                errors={state.errors}
+                />
+          </div>
+          <div className="field">
+          </div>
+          <div className="field">
+            <label className='field-text'>Comments</label>
+            <textarea className='comment-text'  name="comments" />
+          </div>
+          <div className="field">
+          </div>
+          <ReCAPTCHA className='recap'
             sitekey="6LerybcfAAAAAGPWiCYOtDZ9XSLxeLXMjISgqwdf"
             onChange={handleChange}
-        />
-      {captchaDone ?<button type="submit" disabled={state.submitting}>
-        Submit
-      </button> : <button type='button' onClick={() => alert('Please verfy')}></button>}
-    </form>
-    </div>
+            />
+           <div className='field-button'>
+           {captchaDone && <button  type="submit" disabled={state.submitting}>
+                Submit
+            </button>}
+            </div>
+        </div>
   )
 }
 
@@ -61,3 +59,34 @@ export default Contact
 
 // {captchaDone ?}
 // : <button type='button' onClick={() => alert('Please verfy')}></button>
+{/* <form onSubmit={handleSubmit}>
+<label htmlFor="email">
+Email Address
+</label>
+<input
+id="email"
+type="email" 
+name="email"
+/>
+<ValidationError 
+prefix="Email" 
+field="email"
+errors={state.errors}
+/>
+<textarea
+id="message"
+name="message"
+/>
+<ValidationError 
+prefix="Message" 
+field="message"
+errors={state.errors}
+/>
+<ReCAPTCHA
+    sitekey="6LerybcfAAAAAGPWiCYOtDZ9XSLxeLXMjISgqwdf"
+    onChange={handleChange}
+/>
+{captchaDone ?<button type="submit" disabled={state.submitting}>
+Submit
+</button> : <button type='button' onClick={() => alert('Please verfy')}></button>}
+</form> */}
